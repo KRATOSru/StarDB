795ce3d34d237f8ceb52b05326abe54cc6bd953f
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2023-01-07 20:33:30 +0300

    Обработка ошибок в fetch api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const getResource = async (url) => {
   const res = await fetch(url)
+
+  //для отлова ошибок не статусов 200
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+  }
   const body = await res.json()
   return body
 }
 
-getResource('https://swapi.dev/api/people/1/').then((body) => {
-  console.log(body)
-})
+getResource('https://swapi.dev/api/people/1/')
+  .then((body) => {
+    console.log(body)
+  })
+  // для отлова сетевой ошибки, когда нет нета например
+  .catch((err) => {
+    console.error('Not fetch network not vailable', err)
+  })
 
+//аналогичный запрос fetch
 // fetch('https://swapi.dev/api/people/1/')
 //   .then((res) => {
+//     console.log(res)
 //     return res.json()
 //   })
 //   .then((body) => {
