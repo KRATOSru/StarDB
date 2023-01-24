e001af9f7d46e8849e62f38a2eab881505219fdb
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2023-01-25 01:49:16 +0300

    Создан клиент для API

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
-const getResource = async (url) => {
-  const res = await fetch(url)
+class SwapiService {
+  _apiBase = 'https://swapi.dev/api'
 
-  //для отлова ошибок не статусов 200
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`)
+
+    //для отлова ошибок не статусов 200
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+    }
+
+    return await res.json()
   }
-  const body = await res.json()
-  return body
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`)
+    return res.results
+  }
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`)
+    return res.results
+  }
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`)
+    return res.results
+  }
+  getPerson(id) {
+    return this.getResource(`/people/${id}`)
+  }
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`)
+  }
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`)
+  }
 }
 
-getResource('https://swapi.dev/api/people/1/')
-  .then((body) => {
-    console.log(body)
-  })
-  // для отлова сетевой ошибки, когда нет нета например
-  .catch((err) => {
-    console.error('Not fetch network not vailable', err)
-  })
+const swapi = new SwapiService()
 
-//аналогичный запрос fetch
-// fetch('https://swapi.dev/api/people/1/')
-//   .then((res) => {
-//     console.log(res)
-//     return res.json()
-//   })
-//   .then((body) => {
-//     console.log(body)
-//   })
+swapi.getAllPeople().then((people) => {
+  people.forEach((p) => {
+    console.log(p.name)
+  })
+})
+// swapi.getPerson(3).then((p) => {
+//   console.log(p.name)
+// })
