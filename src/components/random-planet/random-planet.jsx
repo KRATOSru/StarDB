5a2da5c963e4132b309a294c5bcc6f43d24abc42
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2023-03-21 13:28:33 +0300

    component didMount

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -13,10 +13,13 @@ export default class RandomPlanet extends Component {
     error: false,
   }
 
-  constructor() {
-    super()
+  componentDidMount() {
     this.updatePlanet()
+    this.interval = setInterval(this.updatePlanet, 2500)
+    //clearInterval(this.interval)
   }
+
+  componentWillUnmount() {}
 
   onPlanetLoaded = (planet) => {
     this.setState({planet, loading: false})
@@ -29,8 +32,8 @@ export default class RandomPlanet extends Component {
     })
   }
 
-  updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2
+  updatePlanet = () => {
+    const id = Math.floor(Math.random() * 25) + 3
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
